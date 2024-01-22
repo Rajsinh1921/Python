224aa0ba36a77234fa5f1c2a470530f7ac2e40b1
Rajsinh Chauhan <rajsinh.chauhan15@gmail.com>
2024-01-21 20:18:10 -0500

    first commit

---
+++ Test.py
@@ -0,0 +1,9 @@
+full_name = "Mahantam"
+age = 90
+gender = "Male"
+strong = True
+print(full_name)
+
+name = input("What is your name? ")
+favcolor = input("What is your fav color? ")
+print( name + ' likes color ' + favcolor)
---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+full_name = "Mahantam"
+age = 90
+gender = "Male"
+strong = True
+print(full_name)
