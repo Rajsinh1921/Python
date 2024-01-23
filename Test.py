abcade89a136a20a7c33742a982848c585debbbd
Rajsinh Chauhan <rajsinh.chauhan15@gmail.com>
2024-01-22 22:31:18 -0500

    For loop

--- Test.py
+++ Test.py
@@ -1,9 +1,74 @@
-full_name = "Mahantam"
-age = 90
-gender = "Male"
-strong = True
-print(full_name)
+# full_name = "Mahantam"
+# age = 90
+# gender = "Male"
+# strong = True
+#  print(full_name)
 
-name = input("What is your name? ")
-favcolor = input("What is your fav color? ")
-print( name + ' likes color ' + favcolor)
+# name = input("What is your name? ")
+# favcolor = input("What is your fav color? ")
+# print( name + ' likes color ' + favcolor)
+
+
+# weight = input("What is your weigh? ")
+# weightInKg = int(weight) * 0.45359237
+# print('Your weight in Kg is ' + str(weightInKg))
+
+# buyer_has_good_creadit = False
+# house_downpayment = 1000000
+
+# if buyer_has_good_creadit:
+#   downpaymnet = house_downpayment * 0.1
+# else: 
+#   downpaymnet = house_downpayment * 0.2
+# print(f'buyer has to put the down payment of {downpaymnet}')
+
+# name = "MSMSMSMSMSMSMSMSMMSMSMSMSMSMSMSMSMSMSMSMSMSSMSMSMS"
+
+# if len(name) < 3:
+#   print('name should be atleast 3 char long')
+# elif len(name) > 50:
+#   print('name can be only 50 char long')
+# else:
+#   print('name looks good')
+# print(len(name))
+
+# Simple if else weight  convertor
+
+# users_weight = int(input('Weight: '))
+# weight_unit = input('Enter the unit in which you want to convert to (L)bs or (k)gs: ').lower()
+
+# if weight_unit == 'l':
+#   users_weight *= 2.20462
+#   print(f'You are {users_weight} pounds')
+# elif weight_unit == 'k':
+#   users_weight *= 0.453592 
+#   print(f'You are {users_weight} kilograms')
+# else:
+#   print("please, enter k or l")
+
+# quit = False
+# car_on = False
+
+# while quit == False:
+#   user_input = input('')
+#   if  user_input == 'help':
+#     print('start - to start car')
+#     print('stop - to stop car')
+#     print('quit - to quit')
+#   elif user_input == 'start':
+#     if not car_on:
+#       print('car started...Ready to go!')
+#       car_on = True
+#     else:
+#       print('car is alredy started')
+#   elif user_input == 'stop':
+#     if car_on:
+#       print('car stopped!')
+#       car_on = False
+#     else:
+#       print('car is stoped started')
+#   elif user_input == 'quit':
+#     quit = True
+#   else:
+#     print("i don't undestand that")
+
